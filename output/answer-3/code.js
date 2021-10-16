8cf3ea0daafa503100a15e5d9821ad70c3d2a414
kritidipto-1234 <kritidiptodgp.ghosh@gmail.com>
2021-10-16 21:56:48 +0530

    some changes

--- output/answer-3/code.js
+++ output/answer-3/code.js
@@ -8,11 +8,19 @@ function swap(arr, i, j) {
     arr[j] = temp;
 }
 
-function sort(data, attribute) {
+function sort(data) {
     const n = data.length;
     for (let i = 0; i < n - 1; i++) {
         for (let j = 0; j < n - i - 1; j++) {
-            if (Number(data[j][attribute]) < Number(data[j + 1][attribute]))
+            if (Number(data[j]["Red Cards"]) < Number(data[j + 1]["Red Cards"]))
+                swap(data, j, j + 1);
+
+            if (
+                Number(data[j]["Red Cards"]) ===
+                    Number(data[j + 1]["Red Cards"]) &&
+                Number(data[j]["Yellow Cards"]) <
+                    Number(data[j + 1]["Yellow Cards"])
+            )
                 swap(data, j, j + 1);
         }
     }
@@ -21,8 +29,7 @@ function sort(data, attribute) {
 async function task() {
     const data = await CSVToJSON().fromFile("../../input/question-3/main.csv");
 
-    sort(data, "Red Cards");
-    sort(data, "Yellow Cards");
+    sort(data);
 
     const csvData = JSONToCSV(data, {
         fields: ["Team", "Yellow Cards", "Red Cards"],
